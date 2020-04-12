a16f9a2d2ebdb9f096c9dcb59b910651c4d5ac07
1600178@eng.asu.edu.eg <1600178@eng.asu.edu.eg>
2020-04-12 12:56:01 +0200

    last edit

--- SJF.py
+++ SJF.py
@@ -1,17 +1,24 @@
 from Scheduler import scheduler
-class sjf(scheduler):
-    def p_sort(self):
+
+
+class Sjf(scheduler):
+
+   def p_sort(self):
+        k = 0
         time_list=[]
         sorted_list=[]
         for i in self.list_process:
             time_list.append(i.burst_time)
         a=zip(time_list, self.list_process)
         sorted(a)
+        for i in a.values:
+            self.list_process[k] = i
+            k += 1
 
-
-
-
-
+    def calc(self):
+        if self.preemptive==0:
+            for i in self.list_process: self.list_element=i
+        else :
 
 
 
