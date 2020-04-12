9bdd920bf8214b2aab781842af4147704ddf6388
Ahmed Emad <1600137@eng.asu.edu.eg>
2020-04-12 12:54:43 +0200

    hh

--- Fcfs.py
+++ Fcfs.py
@@ -1,12 +1,13 @@
 from Scheduler import scheduler
-class fcfs(scheduler) :
+class fcfs (scheduler):
     def calc(self):
         x=[]
         for i in self.list_process:  x.append(i.arrival_time)
         a = zip(x,self.list_process)
         sorted(a)
+        b=0
+        z=[]
         for j,k in a:
-
-
-
-
+            z[b]=k
+            b=b+1
+        self.list_element=z
