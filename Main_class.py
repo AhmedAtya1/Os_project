9fb861a4d659bc4670e6009772ed40f5015ccec9
1600178@eng.asu.edu.eg <1600178@eng.asu.edu.eg>
2020-04-13 16:15:03 +0200

    fcs+sjf debug

--- Main_class.py
+++ Main_class.py
@@ -7,12 +7,12 @@ test=Sjf()
 pro=[]
 
 
-for i in range (4):
+for i in range (6):
     a=process()
     pro.append(a)
-    pro[i].name='p'+str(i)
-pro[0].burst_time,pro[1].burst_time,pro[2].burst_time,pro[3].burst_time=8,4,9,5
-pro[0].arrival_time,pro[1].arrival_time,pro[2].arrival_time,pro[3].arrival_time=0,1,2,3
+    pro[i].name='p'+str(i+1)
+pro[0].burst_time,pro[1].burst_time,pro[2].burst_time,pro[3].burst_time,pro[4].burst_time,pro[5].burst_time=1.5,2,1,15,17,20
+pro[0].arrival_time,pro[1].arrival_time,pro[2].arrival_time,pro[3].arrival_time,pro[4].arrival_time,pro[5].arrival_time=0,1,2,5,6,7
 test.preemptive=1
 test.list_process=pro
 test.calc()
